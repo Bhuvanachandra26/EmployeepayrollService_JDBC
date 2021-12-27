a66911f15826e2e88ff6792f3c085bc3ed10a372
Bhuvanachandra26 <bhuvanachandra26@gmail.com>
2021-12-27 15:27:04 +0530

    Added: code for add payroll details when new employeeDetails added

---
+++ src/main/java/com/bridgelabz/payrolljdbc/Employee.java
@@ -0,0 +1,39 @@
+package com.bridgelabz.payrolljdbc;
+
+import java.sql.Date;
+
+public class Employee {
+    public int Id;
+    public String Name;
+    public float basicPay;
+    public String Start_Date;
+
+    public String getStart_Date() {
+        return Start_Date;
+    }
+    public void setStart_Date(String start_Date) {
+        this.Start_Date = start_Date;
+    }
+    public float getBasicPay() {
+        return basicPay;
+    }
+    public void setBasicPay(float basicPay) {
+        this.basicPay = basicPay;
+    }
+    public int getId() {
+        return Id;
+    }
+    public void setId(int id) {
+        this.Id = id;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+    @Override
+    public String toString() {
+        return "Employee [Id=" + Id + ", Name=" + Name + ", basicPay=" + basicPay+ ", Start_Date=" + Start_Date + "]";
+    }
+}
